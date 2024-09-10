478339daeefaa515364435177471a1fa4c0c57f7
Bukatin Nikita <bukatin@ausferr.ru>
2024-09-10 18:57:42 +0500

    ДЗ №4

--- src/app/components/to-do-list/to-do-list.component.ts
+++ src/app/components/to-do-list/to-do-list.component.ts
@@ -8,12 +8,15 @@ import {Component, OnInit} from '@angular/core';
 export class ToDoListComponent implements OnInit {
   title = "Список задач:";
   isLoading = true;
-  tasks: {id: number, text: string}[] = [
-    { id: 1, text: "task1"},
-    { id: 2, text: "task2"},
-    { id: 3, text: "task3"}
+  tasks: {id: number, text: string, description: string}[] = [
+    { id: 1, text: "Задача 1", description: "Описание первой задачи"},
+    { id: 2, text: "Задача 2", description: "Описание второй задачи"},
+    { id: 3, text: "Задача 3", description: "Описание третьей задачи"}
   ];
   newTask = '';
+  newDescription = '';
+  selectedItemId: number | null = null;
+  foundTask: any;
 
   ngOnInit() {
     setTimeout(() => {this.isLoading = false;}, 500);
@@ -21,12 +24,26 @@ export class ToDoListComponent implements OnInit {
 
   addTask() {
     if (this.newTask.trim()) {
-      this.tasks.push({ id: this.tasks.length + 1, text: this.newTask.trim()});
+      this.tasks.push({ id: this.tasks.length + 1, text: this.newTask.trim(), description: this.newDescription});
       this.newTask = '';
+      this.newDescription = '';
     }
   }
 
   deleteTask(idDel: number) {
     this.tasks = this.tasks.filter(item => item.id !== idDel);
+
+    if (this.selectedItemId === idDel) {
+      this.selectedItemId = null;
+    }
+  }
+
+  selectItem(selectedId: number) {
+    this.selectedItemId = selectedId;
+  }
+
+  getSelectedTaskDescription(): string | undefined {
+    const selectedTask = this.tasks.find(task => task.id === this.selectedItemId);
+    return selectedTask ? selectedTask.description : undefined;
   }
 }
